ba383531d8245686beb27eab387cd2b14044e18d
YGKr <ygkr32@gmail.com>
2016-11-03 21:35:49 +0900

    Image Update
    GetImageUser Add Update

---
+++ source/Image.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+
+namespace NicoCs
+{
+    namespace Image
+    {
+        class User
+        {
+            public string id;
+            public string NickName;
+
+            public void GetUserData(string ID)
+            {
+                string URL = "http://seiga.nicovideo.jp/api/user/info?id=" + ID;
+                string NameBuffer = "";
+
+                XmlTextReader reader = new XmlTextReader(URL);
+
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+
+                            NameBuffer = reader.Name;
+
+                            break;
+
+                        case XmlNodeType.Text:
+
+                            if (NameBuffer == "id") this.id = reader.Value;
+
+                            if (NameBuffer == "nickname") this.NickName = reader.Value;
+
+                            break;
+
+                        default: break;
+                    }
+                }
+            }
+        }
+    }
+}
